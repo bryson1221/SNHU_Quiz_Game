5b4f49238b837011fe7bf5796b3dfcdaa8d3be4b
bryson1221 <bryson.anderson@snhu.edu>
2022-11-04 19:36:33 -0500

    5 quiz question

--- SNHU_Quiz_Game.py
+++ SNHU_Quiz_Game.py
@@ -1,1 +1,44 @@
+print("Welcome to my SNHU Game!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play ")
+
+answer = input("What city is SNHU located? ")
+
+if answer == "Manchester":
+    print('Correct!')
+else: 
+    print("Incorrect!")
+
+answer = input("What year was SNHU founded? ")
+
+if answer == "1932":
+    print('Correct!')
+else: 
+    print("Incorrect!")
+
+answer = input("What is the full name of the SNHU mascot? ")
+
+if answer == "Petey Penmen":
+    print('Correct!')
+else: 
+    print("Incorrect!")
+
+answer = input("Is SNHU a dry campus? ")
+
+if answer == "yes":
+    print('Correct!')
+else: 
+    print("Incorrect!")
+
+answer = input("Is SNHU a Yellow Ribbon school? ")
+
+if answer == "yes":
+    print('Correct!')
+else: 
+    print("Incorrect!")
 
